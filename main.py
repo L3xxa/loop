658c1252b26370772098911452d9feb1d75a8576
Cycle <alekseiholovati@gmail.com>
2024-10-23 16:53:29 +0300

    comlete ex_1

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-if __name__ == '__main__':
-    pass
+# Example 1
+def simple_numbers(a,b):
+    prime_numbers=[]
+    for i in range(a,b+1):
+        k=0
+        for j in range(1,i+1):
+            if i%j==0:
+                k+=1
+        if k==2:
+            prime_numbers.append(i)
+    print(prime_numbers)
+
+a = int(input( "Enter a number: " ))
+b = int(input( "Enter a number: " ))
+simple_numbers(a,b)
+
+
+
+
--- test.py
+++ test.py
@@ -3,3 +3,4 @@ import unittest
 
 class Test(unittest.TestCase):
     pass
+
