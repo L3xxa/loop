bf6dbf29c7a59391557a0266cb8b3524b1674022
Cycle <alekseiholovati@gmail.com>
2024-10-25 00:33:31 +0300

    complete ex_1

--- functions.py
+++ functions.py
@@ -10,3 +10,26 @@ def sum_digits (num):
     for i in str(num):
         sum += int(i)
     return sum
+
+def middle_arithmetic (num):
+    mid = 0
+    count = 0
+    for i in str(num):
+        mid += int(i)
+        count += 1
+
+    if count == 0:
+        return 0
+    middle = mid/count
+    return middle
+
+
+def zeros (num):
+    zer = 0
+    for i in str(num):
+        if i == '0':
+            zer += 1
+    return zer
+
+
+
--- main.py
+++ main.py
@@ -1,20 +1,23 @@
-from functions import count_digits, sum_digits
+from functions import count_digits, sum_digits,middle_arithmetic,zeros
 
 a = int(input("Введіть число : "))
 par = int(input("Виберіть функцію:\n"
-                "1:Дізнатися кількість чисел:\n"
+                "1:Дізнатися к-ть чисел:\n"
                 "2:Дізнатися суму чисел:\n"
-                "1-2: "))
+                "3:Дізнатися середньоарифметичне:\n"
+                "4:Дізнатися к-ть нулів:\n"
+                "1-4: "))
 
 result_count = count_digits(a)
 result_sum = sum_digits(a)
+result_mid = middle_arithmetic(a)
+result_zero = zeros(a)
 
 if par == 1:
-    print (f'Кількість чисел = {result_count}')
+    print (f'К-ть чисел = {result_count}')
 elif par == 2:
     print(f'Сума чисел = {result_sum}')
-
-
-
-
-
+elif par == 3 :
+    print(f'Середньоарифметичне числа = {result_mid} ')
+elif par == 4:
+    print(f'К-ть нулів = {result_zero}')
