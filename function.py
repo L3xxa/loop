cc65c86fa15f4ac6f486e99e64590e013f908a22
Cycle <alekseiholovati@gmail.com>
2024-10-26 13:04:18 +0300

    complete ex_4

---
+++ function.py
@@ -0,0 +1,15 @@
+def romb():
+
+    for i in range(0,7):
+        print("   "*(7-i-1),end=" ")
+        for j in range(0,7):
+            if i >= j:
+                print(f'[{i},{j}]',end=" ")
+        print()
+
+    for i in range(6,-1,-1):
+        print("   "*(7-i-1),end=" ")
+        for j in range(6,-1,-1):
+            if i >= j:
+                print(f'[{i},{j}]',end=" ")
+        print()
---
+++ main.py
@@ -0,0 +1,3 @@
+from function import romb
+
+romb()
