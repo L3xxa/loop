254405acfbc6362b919eb71e7a1a6793dd37ddd0
Cycle <alekseiholovati@gmail.com>
2024-10-27 23:13:17 +0200

    trust fix bag

--- function.py
+++ function.py
@@ -4,12 +4,12 @@ def romb():
         print("   "*(7-i-1),end=" ")
         for j in range(0,7):
             if i >= j:
-                print(f'[{i},{j}]',end=" ")
+                print(f'*',end="     ")
         print()
 
     for i in range(6,-1,-1):
         print("   "*(7-i-1),end=" ")
         for j in range(6,-1,-1):
             if i >= j:
-                print(f'[{i},{j}]',end=" ")
+                print(f'*',end="     ")
         print()
