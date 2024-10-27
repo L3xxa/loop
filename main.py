60c7aef7f6993865acaf23767f7c6ab9ce2ab077
Cycle <alekseiholovati@gmail.com>
2024-10-27 22:59:47 +0200

    complete ex_3

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-if __name__ == '__main__':
-    pass
+from function import function1, function2, function3
+par = int(input("Виберіть рівень складності: \n"
+                "1: Програма 1-го класу: \n"
+                "2: Програма 7-го класу: \n"
+                "3: Вища математика: \n"
+                "Ваш вибір=>"))
+if par == 1:
+    function1()
+elif par == 2:
+    function2()
+elif par == 3:
+    function3()
+else:
+    print("Йой, я так не робив, дивись на варіанти")
